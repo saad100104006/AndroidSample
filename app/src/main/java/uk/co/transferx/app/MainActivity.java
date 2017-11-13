f9d6705104f3e2c67f534e10416f2448c54ca384
Sergey Milevskiy <sergey.milevskiy@xtb.com>
2017-11-13 08:50:22 +0100

    git init, configure build.gradle and remote repo

---
+++ app/src/main/java/uk/co/transferx/app/MainActivity.java
@@ -0,0 +1,19 @@
+package uk.co.transferx.app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by smilevkiy on 13.11.17.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
