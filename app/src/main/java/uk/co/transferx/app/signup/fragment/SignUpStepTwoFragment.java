114c36650af5393ce6d94f9e991e266c631bced9
Siarhei Mileuski <siarhei.mileuski@mobica.com>
2017-12-04 12:11:30 +0100

    Delete unnecessary stuff

--- app/src/main/java/uk/co/transferx/app/signup/fragment/SignUpStepTwoFragment.java
+++ app/src/main/java/uk/co/transferx/app/signup/fragment/SignUpStepTwoFragment.java
@@ -2,12 +2,9 @@ package uk.co.transferx.app.signup.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TextInputLayout;
-import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import uk.co.transferx.app.BaseFragment;
 import uk.co.transferx.app.R;
@@ -24,21 +21,12 @@ public class SignUpStepTwoFragment extends BaseFragment {
         return SignUpStepTwoFragment.class.getSimpleName();
     }
 
-    private TextInputLayout firstInputLayout;
-    private TextInputLayout secondInputLayout;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.sign_up_step_two_fragment_layout, container, false);
-        firstInputLayout = view.findViewById(R.id.first_layout);
-        secondInputLayout = view.findViewById(R.id.second_layout);
-        ((TextView) view.findViewById(R.id.title)).setText(getString(R.string.title_two));
-        firstInputLayout.setHint(getString(R.string.email));
-        firstInputLayout.getEditText().setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
-        secondInputLayout.setHint(getString(R.string.password));
-        secondInputLayout.getEditText().setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
         view.findViewById(R.id.next_step).setOnClickListener(v -> ((SignUpActivity) getActivity()).showNextOrPreviousFragment(2));
         return view;
     }
