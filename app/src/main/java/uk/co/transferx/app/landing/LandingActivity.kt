4a26844b305bd40486bc45ab4bc486f0f281bd7a
catalinghita8 <catalinghita8@gmail.com>
2018-09-12 21:27:43 +0300

    Create landing screen UI
    Solves DMVP-116

---
+++ app/src/main/java/uk/co/transferx/app/landing/LandingActivity.kt
@@ -0,0 +1,22 @@
+package uk.co.transferx.app.landing
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import uk.co.transferx.app.BaseActivity
+import uk.co.transferx.app.R
+
+class LandingActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+}
---
+++ app/src/main/java/uk/co/transferx/app/landing/fragment/LandingFragment.kt
@@ -0,0 +1,21 @@
+package uk.co.transferx.app.landing.fragment
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import uk.co.transferx.app.R
+
+
+class LandingFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_landing, container, false)
+    }
+
+}
