17bcdf4073d457002ff3a0c1773dc24e7e9334e9
Sandeep Gupta <>
2018-09-18 21:09:30 +0530

    Home Screen's Fragment UI updtaes, Cutomizing bottom navigation bar

--- app/src/main/java/uk/co/transferx/app/ui/homescreen/HomeActivity.kt
+++ app/src/main/java/uk/co/transferx/app/ui/homescreen/HomeActivity.kt
@@ -2,41 +2,93 @@ package uk.co.transferx.app.ui.homescreen
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.content.ContextCompat
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_home.*
 import uk.co.transferx.app.R
 import uk.co.transferx.app.ui.base.BaseActivity
 import uk.co.transferx.app.ui.homescreen.fragments.FragActivity
 import uk.co.transferx.app.ui.homescreen.fragments.FragRecipients
 import uk.co.transferx.app.ui.homescreen.fragments.FragSettings
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
+import android.graphics.drawable.Drawable
+
+
 
 class HomeActivity : BaseActivity() {
-
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                callFragmentActivity()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                callFragmentRecipients()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                callFragmentSettings()
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-        BottomNavigationHelper.disableShiftMode(navigation)
+        val tvActivity = findViewById<TextView>(R.id.tv_activity)
+        val tvRecipients = findViewById<TextView>(R.id.tv_recipients)
+        val tvSettings = findViewById<TextView>(R.id.tv_settings)
+
+        val barActivity = findViewById<View>(R.id.view_activity_bar)
+        val barRecipients = findViewById<View>(R.id.view_recipients_bar)
+        val barSettings = findViewById<View>(R.id.view_settings_bar)
+
+        tvActivity.setOnClickListener(View.OnClickListener {
+            tvActivity.setTextColor(ContextCompat.getColor(this, R.color.amber))
+            setTextViewDrawableColor(tvActivity, R.color.amber)
+            barActivity.setBackgroundColor(ContextCompat.getColor(this, R.color.amber))
+
+            tvRecipients.setTextColor(ContextCompat.getColor(this, R.color.grey_text))
+            setTextViewDrawableColor(tvRecipients, R.color.grey_text)
+            barRecipients.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent))
+
+            tvSettings.setTextColor(ContextCompat.getColor(this, R.color.grey_text))
+            setTextViewDrawableColor(tvSettings, R.color.grey_text)
+            barSettings.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent))
+
+            callFragmentActivity()
+        })
+
+        tvRecipients.setOnClickListener(View.OnClickListener {
+            tvActivity.setTextColor(ContextCompat.getColor(this, R.color.grey_text))
+            setTextViewDrawableColor(tvActivity, R.color.grey_text)
+            barActivity.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent))
+
+            tvRecipients.setTextColor(ContextCompat.getColor(this, R.color.amber))
+            setTextViewDrawableColor(tvRecipients, R.color.amber)
+            barRecipients.setBackgroundColor(ContextCompat.getColor(this, R.color.amber))
+
+            tvSettings.setTextColor(ContextCompat.getColor(this, R.color.grey_text))
+            setTextViewDrawableColor(tvSettings, R.color.grey_text)
+            barSettings.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent))
+
+            callFragmentRecipients()
+        })
+
+        tvSettings.setOnClickListener(View.OnClickListener {
+            tvActivity.setTextColor(ContextCompat.getColor(this, R.color.grey_text))
+            setTextViewDrawableColor(tvActivity, R.color.grey_text)
+            barActivity.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent))
+
+            tvRecipients.setTextColor(ContextCompat.getColor(this, R.color.grey_text))
+            setTextViewDrawableColor(tvRecipients, R.color.grey_text)
+            barRecipients.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent))
+
+            tvSettings.setTextColor(ContextCompat.getColor(this, R.color.amber))
+            setTextViewDrawableColor(tvSettings, R.color.amber)
+            barSettings.setBackgroundColor(ContextCompat.getColor(this, R.color.amber))
+
+            callFragmentSettings()
+        })
 
         callFragmentActivity()
+    }
+
+    private fun setTextViewDrawableColor(textView: TextView, color: Int) {
+        for (drawable in textView.compoundDrawables) {
+            if (drawable != null) {
+                drawable.colorFilter = PorterDuffColorFilter(ContextCompat.getColor(this, color), PorterDuff.Mode.SRC_IN)
+            }
+        }
     }
 
     internal fun callFragmentActivity() {
--- app/src/main/java/uk/co/transferx/app/ui/homescreen/fragments/FragRecipients.kt
+++ app/src/main/java/uk/co/transferx/app/ui/homescreen/fragments/FragRecipients.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 
 import uk.co.transferx.app.R
 import uk.co.transferx.app.ui.base.BaseFragment
@@ -11,7 +13,22 @@ import uk.co.transferx.app.ui.base.BaseFragment
 class FragRecipients : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.frag_recipients, container, false)
+        val view = inflater.inflate(R.layout.frag_recipients, container, false)
+
+        val imgSearch = view.findViewById<ImageView>(R.id.img_search)
+        imgSearch.setOnClickListener {  }
+
+        val imgAddRecipient = view.findViewById<ImageView>(R.id.img_add_recipient)
+        imgAddRecipient.setOnClickListener { callAddRecipient() }
+
+        val tvAddRecipient = view.findViewById<TextView>(R.id.tv_add_recipient)
+        tvAddRecipient.setOnClickListener { callAddRecipient() }
+
+        return view
+    }
+
+    internal fun callAddRecipient() {
+
     }
 
     override fun tagName(): String {
