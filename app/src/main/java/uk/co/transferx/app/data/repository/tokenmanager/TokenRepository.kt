e7b6a234adb8ef5c774447835bf1cc5bf7f46419
Sandeep Gupta <sandeep.droid87@gmail.com>
2018-09-30 14:58:58 +0530

    cleanup

--- app/src/main/java/uk/co/transferx/app/data/repository/tokenmanager/TokenRepository.kt
+++ app/src/main/java/uk/co/transferx/app/data/repository/tokenmanager/TokenRepository.kt
@@ -23,10 +23,10 @@ class TokenRepository @Inject constructor(private val sharedPreferences: SharedP
     fun getToken(): TokenEntity {
         Log.d("Serge", "ReadToken " + sharedPreferences.getString(KEY_TOKEN, "").takeLast(7))
         return TokenEntity(
-           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfaWQiOiIwMTY2OTBmNi1iYzhjLTQ3NDMtODQzYS0zN2FkODdhMzUzMDYiLCJhdG9rZW5faWQiOiI3NzE0OTk4Mi1iODJjLTRiMGEtYmU1Mi0zOTFkNDkxMDNlNDEiLCJpYXQiOjE1MzgyOTYwNzl9.vZ4vOsO63T05i2DqCjA1vI6N5auYNOwHlm1sYqXNF-8", //sharedPreferences.getString(KEY_TOKEN, ""),
+            sharedPreferences.getString(KEY_TOKEN, ""),
             sharedPreferences.getLong(KEY_EXPIRES_IN, DEFAULT_TIMESTAMP),
             sharedPreferences.getLong(KEY_CREATED, DEFAULT_TIMESTAMP),
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRfaWQiOiIwMTY2OTBmNi1iYzhjLTQ3NDMtODQzYS0zN2FkODdhMzUzMDYiLCJyZWZyZXNoX3Rva2VuX2lkIjoiZjAwNjYzMzYtZTgzOS00OWI1LTg1YjItNmVmMzAwMWYzODE1IiwiaWF0IjoxNTM4Mjk2MDc5fQ.oz9INF_RFbvk9sB_luZw39TakvaZhJas7A0sE5765G4"//sharedPreferences.getString(KEY_REFRESH_TOKEN, "")
+            sharedPreferences.getString(KEY_REFRESH_TOKEN, "")
         )
     }
 
