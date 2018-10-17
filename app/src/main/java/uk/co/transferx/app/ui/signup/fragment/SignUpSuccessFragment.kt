0b26ebb43dd9f0d1fa18a66fd150e9df667fd56a
catalinghita8 <catalinghita8@gmail.com>
2018-10-17 14:55:55 +0300

    Update UI and make changes due to sync with temp/1.0.0

--- app/src/main/java/uk/co/transferx/app/ui/signup/fragment/SignUpSuccessFragment.kt
+++ app/src/main/java/uk/co/transferx/app/ui/signup/fragment/SignUpSuccessFragment.kt
@@ -13,7 +13,7 @@ import org.jetbrains.anko.intentFor
 import uk.co.transferx.app.R
 import uk.co.transferx.app.TransferXApplication
 import uk.co.transferx.app.ui.base.BaseFragment
-import uk.co.transferx.app.ui.homescreen.HomeActivity
+import uk.co.transferx.app.ui.homescreen.MainActivity
 import uk.co.transferx.app.ui.recipients.addrecipients.AddRecipientsActivity
 import uk.co.transferx.app.ui.recipients.addrecipients.Mode
 import uk.co.transferx.app.ui.settings.profile.wallet.AddCardActivity
@@ -70,7 +70,7 @@ class SignUpSuccessFragment : BaseFragment(), SignUpSuccessPresenter.SignUpSucce
     }
 
     override fun showMainScreen() {
-        context?.startActivity(context?.intentFor<HomeActivity>())
+        context?.startActivity(context?.intentFor<MainActivity>())
         activity?.finish()
     }
 
