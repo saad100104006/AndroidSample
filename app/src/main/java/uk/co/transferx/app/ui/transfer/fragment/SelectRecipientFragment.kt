fe04f7a97aa6ab7ebabc1b80e14d8bd2c8181e01
catalinghita8 <catalinghita8@gmail.com>
2018-11-15 15:53:24 +0200

    Create adapters and xml resources

--- app/src/main/java/uk/co/transferx/app/ui/transfer/fragment/SelectRecipientFragment.kt
+++ app/src/main/java/uk/co/transferx/app/ui/transfer/fragment/SelectRecipientFragment.kt
@@ -2,22 +2,32 @@ package uk.co.transferx.app.ui.transfer.fragment
 
 
 import android.os.Bundle
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_select_recipient.*
 
 import uk.co.transferx.app.R
 import uk.co.transferx.app.TransferXApplication
+import uk.co.transferx.app.data.pojo.Recipient
 import uk.co.transferx.app.ui.base.BaseFragment
+import uk.co.transferx.app.ui.transfer.adapters.RecipientsAdapter
 import uk.co.transferx.app.ui.transfer.presenters.SelectRecipientPresenter
 import javax.inject.Inject
 
 /**
  * Created by Catalin Ghita on 15.11.2018.
  */
-class SelectRecipientFragment : BaseFragment(), SelectRecipientPresenter.SelectRecipientView {
+class SelectRecipientFragment : BaseFragment(), SelectRecipientPresenter.SelectRecipientView,
+        RecipientsAdapter.ItemClickListener {
+    internal lateinit var adapter: RecipientsAdapter
     @Inject
     lateinit var presenter: SelectRecipientPresenter
+
+    override fun onItemClick(view: View, data: Recipient) {
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         (activity?.application as TransferXApplication).appComponent.inject(this)
@@ -28,10 +38,19 @@ class SelectRecipientFragment : BaseFragment(), SelectRecipientPresenter.SelectR
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_select_recipient, container, false)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        adapter = RecipientsAdapter(context!!)
+        adapter.setClickListener(this)
+
+        val layoutManager = LinearLayoutManager(activity)
+        recyclerviewHistory.setLayoutManager(layoutManager)
+        recyclerviewHistory.setItemAnimator(DefaultItemAnimator())
+        recyclerviewHistory.setAdapter(adapter)
     }
 
     override fun onResume() {
@@ -46,6 +65,10 @@ class SelectRecipientFragment : BaseFragment(), SelectRecipientPresenter.SelectR
 
     override fun goToNextStep() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showRecipientList(recipients: List<Recipient>) {
+        adapter.setRecipients(recipients)
     }
 
     override fun goToWelcome() {
--- app/src/main/java/uk/co/transferx/app/ui/transfer/presenters/SelectRecipientPresenter.kt
+++ app/src/main/java/uk/co/transferx/app/ui/transfer/presenters/SelectRecipientPresenter.kt
@@ -1,5 +1,6 @@
 package uk.co.transferx.app.ui.transfer.presenters
 
+import uk.co.transferx.app.data.pojo.Recipient
 import uk.co.transferx.app.data.repository.recipientsrepository.RecipientRepository
 import uk.co.transferx.app.ui.base.BasePresenter
 import uk.co.transferx.app.ui.base.UI
@@ -13,9 +14,43 @@ constructor(private val recipientRepository: RecipientRepository) :
         super.attachUI(ui)
 
         // Get recipients
+        val list = ArrayList<Recipient>()
+        val recipient1 =   Recipient.Builder()
+                .firstname("Anna")
+                .lastname("Frank")
+                .country("XX")
+                .phone("39348")
+                .build()
+        val recipient2 =   Recipient.Builder()
+                .firstname("Britney")
+                .lastname("Nest")
+                .country("XX")
+                .phone("39348")
+                .build()
+        val recipient3 =   Recipient.Builder()
+                .firstname("John")
+                .lastname("Wick")
+                .country("XX")
+                .phone("39348")
+                .build()
+        val recipient4 =   Recipient.Builder()
+                .firstname("Welney")
+                .lastname("Pardy")
+                .country("XX")
+                .phone("39348")
+                .build()
+
+        list.add(recipient1)
+        list.add(recipient2)
+        list.add(recipient3)
+        list.add(recipient4)
+
+        this.ui.showRecipientList(list)
     }
 
     interface SelectRecipientView: UI {
         fun goToNextStep()
+
+        fun showRecipientList(recipients: List<Recipient>)
     }
 }
